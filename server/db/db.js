c4ffe400de3f348c76faaedb597e0e61475f99eb
ankush_singh <ankush25kumar@gmail.com>
2021-01-03 14:07:11 +0530

    mergeconflict

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgresql://ankushproject:ankush@localhost:5432/ankushdb', {
+  process.env.DATABASE_URL || 'postgresql://akshay:123456@localhost:5432/akshay', {
     logging: false
   }
 )
